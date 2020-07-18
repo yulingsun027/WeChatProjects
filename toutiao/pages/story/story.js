45a45ccbe5165bd48ff58e36857ab025f00b868c
yuling sun <yulingsun027@hotmail.com>
2020-07-18 17:29:57 +0800

    toutiao skeleton

--- toutiao/pages/story/story.js
+++ toutiao/pages/story/story.js
@@ -5,62 +5,41 @@ Page({
    * Page initial data
    */
   data: {
-
+  story:{}
   },
 
   /**
    * Lifecycle function--Called when page load
    */
+  formSubmit: function (event) {
+
+    let name = event.detail.value.name;
+    let content = event.detail.value.content;
+
+    
+
+   let story = {
+      name: name,
+      content : content
+    };
+
+
+    let request = {
+      url:'https://cloud.minapp.com/oserve/v1/table/84988/record/',
+      method: 'POST',
+      header: {'Authorization':'Bearer 7a82a2b76c38e309ae34ff3c83c87f8409748b0e'},
+      data : story,
+      success () {
+        wx.navigateTo({
+          url: '/pages/index/index'
+        })
+      }
+    }
+    wx.request(request)
+  },
+  
   onLoad: function (options) {
-
   },
 
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
-  }
+  
 })
